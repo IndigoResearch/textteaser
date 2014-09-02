91d9d56ec0fdc94697a3c638024bd306f222a06b
Jolo Balbin <jolo@jpbalb.in>
2014-09-02 20:38:59 +0800

    added dbs

--- summarizer.py
+++ summarizer.py
@@ -16,19 +16,26 @@ class Summarizer:
 
   def getTopKeywords(self, keywords, wordCount, source, category):
     # Add getting top keywords in the database here
+    for keyword in keywords:
+      articleScore = 1.0 * keyword['count'] / wordCount
+      keyword['totalScore'] = articleScore * 1.5
+
     return keywords
 
   def computeScore(self, sentences, titleWords, topKeywords):
     keywordList = [keyword['word'] for keyword in topKeywords]
 
     for sentence in sentences:
-      sbsFeature = self.sbs(sentence, topKeywords, keywordList)
+      sentence = self.parser.removePunctations(sentence)
+      words = self.parser.splitWords(sentence)
 
-      print sbsFeature
+      sbsFeature = self.sbs(words, topKeywords, keywordList)
+      dbsFeature = self.dbs(words, topKeywords, keywordList)
 
-  def sbs(self, sentence, topKeywords, keywordList):
-    sentence = self.parser.removePunctations(sentence)
-    words = self.parser.splitWords(sentence)
+      print 'sbs: %f' % sbsFeature
+      print 'dbs: %f' % dbsFeature
+
+  def sbs(self, words, topKeywords, keywordList):
     score = 0.0
 
     if len(words) == 0:
@@ -42,13 +49,28 @@ class Summarizer:
         index = keywordList.index(word)
 
       if index > -1:
-        score += topKeywords[index]['count']
+        score += topKeywords[index]['totalScore']
 
     return 1.0 / abs(len(words)) * score
 
-  def dbs(self, sentence, topKeywords):
-    None
+  def dbs(self, words, topKeywords, keywordList):
+    k = len(list(set(words) & set(keywordList))) + 1
+    summ = 0.0
+    firstWord = {}
+    secondWord = {}
 
-# Blank commit
-# Testing my mechanical keyboard
-# So awesome
+    for i, word in enumerate(words):
+      if word in keywordList:
+        index = keywordList.index(word)
+
+        if firstWord == {}:
+          firstWord = {'i': i, 'score': topKeywords[index]['totalScore']}
+        else:
+          secondWord = firstWord
+          firstWord = {'i': i, 'score': topKeywords[index]['totalScore']}
+          distance = firstWord['i'] - secondWord['i']
+
+          summ += (firstWord['score'] * secondWord['score']) / (distance ** 2)
+
+
+    return (1.0 / k * (k + 1.0)) * summ
